c487a15248e883fc7582bd25001ce05461adfddb
Jake Lewis <jake@globus.org>
2024-04-10 16:02:53 -0500

    chore: use local file instead of gist linked file

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -7,7 +7,7 @@ function configureEditor(monaco: Monaco) {
     validate: true,
     schemas: [
       {
-        uri: "https://gist.githubusercontent.com/jbottigliero/e7276bbdb5e56974e49ea69f91660c4a/raw/690956b1102341232741e8aeb1e7ce80adc7d1cd/flow_definition_schema.json",
+        uri: `${window.location.origin}${process.env.NEXT_PUBLIC_BASE_PATH}/schemas/flow_definition_schema.json`,
         fileMatch: ["*"],
       },
     ],
