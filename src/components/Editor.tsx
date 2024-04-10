cea01ecd239d982d656d4637de9c193c1c9d1e4d
jakeglobus <76002510+jakeglobus@users.noreply.github.com>
2024-04-10 17:00:31 -0500

    feat: load and use Flow definition schema for autocomplete (#30)
    [sc-32955]

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,27 @@
 "use client";
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor, { type Monaco } from "@monaco-editor/react";
+
+function configureEditor(monaco: Monaco) {
+  monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+    enableSchemaRequest: true,
+    validate: true,
+    schemaValidation: "ignore",
+    schemas: [
+      {
+        uri: `${window.location.origin}${process.env.NEXT_PUBLIC_BASE_PATH}/schemas/flow_definition_schema.json`,
+        fileMatch: ["*"],
+      },
+    ],
+  });
+}
 
 export default function Editor(props: any[]) {
-  return <MonacoEditor defaultLanguage="json" language="json" {...props} />;
+  return (
+    <MonacoEditor
+      defaultLanguage="json"
+      language="json"
+      beforeMount={configureEditor}
+      {...props}
+    />
+  );
 }
