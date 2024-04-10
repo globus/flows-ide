be92ab7ddfaf6282034f4127bdcf0f188e8c7686
Jake Lewis <jake@globus.org>
2024-04-10 15:14:30 -0500

    feat(FlowsIDE): load and use Flow definition schema for validation and autocomplete
    [sc-32955]

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,26 @@
 "use client";
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor, { type Monaco } from "@monaco-editor/react";
+
+function configureEditor(monaco: Monaco) {
+  monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+    enableSchemaRequest: true,
+    validate: true,
+    schemas: [
+      {
+        uri: "https://gist.githubusercontent.com/jbottigliero/e7276bbdb5e56974e49ea69f91660c4a/raw/690956b1102341232741e8aeb1e7ce80adc7d1cd/flow_definition_schema.json",
+        fileMatch: ["*"],
+      },
+    ],
+  });
+}
 
 export default function Editor(props: any[]) {
-  return <MonacoEditor defaultLanguage="json" language="json" {...props} />;
+  return (
+    <MonacoEditor
+      defaultLanguage="json"
+      language="json"
+      beforeMount={configureEditor}
+      {...props}
+    />
+  );
 }
