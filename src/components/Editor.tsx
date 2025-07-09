3a0f36aa0837d55da1235489bf239df396c8202f
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2025-07-09 11:45:11 -0500

    feat: adds "Input Schema" validation (#134)

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -7,16 +7,30 @@ import MonacoEditor, {
 import { ValidateButton } from "./Validate";
 import { Box } from "@chakra-ui/react";
 
+export const MODES = {
+  DEFINITION: "DEFINITION",
+  INPUT_SCHEMA: "INPUT_SCHEMA",
+} as const;
+
+export type SupportedModes = keyof typeof MODES;
+
+function isDefinitionMode(settings?: InteralSettings) {
+  return settings?.mode === MODES.DEFINITION || !settings?.mode;
+}
+
 type InteralSettings = {
-  enableExperimentalValidation: boolean;
+  mode?: SupportedModes;
+  enableExperimentalValidation?: boolean;
 };
 
 function configureEditor(
   monaco: Monaco,
   settings: InteralSettings = {
+    mode: MODES.DEFINITION,
     enableExperimentalValidation: true,
   },
 ) {
+  console.log("Configuring editor with settings", settings);
   monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
     enableSchemaRequest: true,
     validate: true,
@@ -26,7 +40,11 @@ function configureEditor(
     schemas: [
       {
         uri: `${window.location.origin}${process.env.NEXT_PUBLIC_BASE_PATH}/schemas/flow_definition_schema.json`,
-        fileMatch: ["*"],
+        fileMatch: ["definition.json"],
+      },
+      {
+        uri: `${window.location.origin}${process.env.NEXT_PUBLIC_BASE_PATH}/schemas/input_schema_schema.json`,
+        fileMatch: ["input-schema.json"],
       },
     ],
   });
@@ -46,9 +64,11 @@ export default function Editor(
           }}
           {...props}
         />
-        <Box pos="absolute" bottom={5} right={10} zIndex={10}>
-          <ValidateButton />
-        </Box>
+        {isDefinitionMode(props.settings) && (
+          <Box pos="absolute" bottom={5} right={10} zIndex={10}>
+            <ValidateButton />
+          </Box>
+        )}
       </Box>
     </>
   );
