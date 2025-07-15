82b8d47a5832462dae0d9770f2a0d311027ca2d8
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2025-07-15 09:15:34 -0500

    fix: ClipboardCopyButton.tsx

--- src/components/ClipboardCopyButton.tsx
+++ src/components/ClipboardCopyButton.tsx
@@ -1,32 +1,32 @@
 "use client";
 
 import { useEditorStore } from "@/stores/editor";
-import { Button } from "@chakra-ui/react";
-import { useClipboard } from "@chakra-ui/react";
+import { Button, useClipboard } from "@chakra-ui/react";
 import { compressToEncodedURIComponent } from "lz-string";
+import { useEffect } from "react";
 import { LuCheck } from "react-icons/lu";
 
 export function ClipboardCopyButton() {
-  const editorStore = useEditorStore();
-  const clipboard = useClipboard("", 2000);
+  const definition = useEditorStore((state) => state.definition);
+  const schema = useEditorStore((state) => state.schema);
+  const { onCopy, hasCopied, setValue } = useClipboard("");
+
+  useEffect(() => {
+    setValue(
+      `${globalThis.document.location.origin}/flows-ide?d=${compressToEncodedURIComponent(
+        JSON.stringify(definition),
+      )}&s=${compressToEncodedURIComponent(JSON.stringify(schema))}`,
+    );
+  }, [definition, schema, setValue]);
+
   return (
-    globalThis.document && (
-      <Button
-        size="xs"
-        colorScheme={!clipboard.hasCopied ? "blue" : "green"}
-        onClick={() =>
-          clipboard.onCopy(
-            `${globalThis.document.location.origin}/flows-ide?d=${compressToEncodedURIComponent(
-              JSON.stringify(editorStore.definition),
-            )}&s=${compressToEncodedURIComponent(JSON.stringify(editorStore.schema))}`,
-          )
-        }
-      >
-        {clipboard.hasCopied
-          ? "Copied to clipboard!"
-          : "Copy link to share this Flow"}
-        {clipboard.hasCopied ? <LuCheck /> : null}
-      </Button>
-    )
+    <Button
+      size="xs"
+      colorScheme={!hasCopied ? "blue" : "green"}
+      onClick={onCopy}
+      leftIcon={hasCopied ? <LuCheck /> : undefined}
+    >
+      {hasCopied ? "Shareable URL Copied to Clipboard" : "Share"}
+    </Button>
   );
 }
