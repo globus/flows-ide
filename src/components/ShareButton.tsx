634a6fb2532cd0f8255f20185d458a0e56960bff
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2025-08-05 09:36:49 -0500

    feat: Adds support for encoded, gzipped, query parameters

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,42 +1,43 @@
 "use client";
 
 import { useEditorStore } from "@/stores/editor";
-import { CopyButton, Button } from "@mantine/core";
+import { COMPRESSION_METHODS, encode } from "@/utils/compression";
+import { Button } from "@mantine/core";
+import { useClipboard } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
-import { compressToEncodedURIComponent } from "lz-string";
+import { useEffect } from "react";
 import { LuCheck, LuShare } from "react-icons/lu";
 
 export function ShareButton() {
+  const clipboard = useClipboard();
   const definition = useEditorStore((state) => state.definition);
   const schema = useEditorStore((state) => state.schema);
 
   const host = globalThis?.document?.location.origin;
 
+  useEffect(() => {
+    if (clipboard.copied) {
+      notifications.show({
+        id: "clipboard-copy",
+        message: "Shareable URL copied to clipboard.",
+        color: "green",
+        position: "top-center",
+      });
+    }
+  }, [clipboard.copied]);
+
   return (
-    <CopyButton
-      value={`${host}/flows-ide?d=${compressToEncodedURIComponent(
-        JSON.stringify(definition),
-      )}&s=${compressToEncodedURIComponent(JSON.stringify(schema))}`}
-    >
-      {({ copied, copy }) => {
-        if (copied) {
-          notifications.show({
-            id: "clipboard-copy",
-            message: "Shareable URL copied to clipboard.",
-            color: "green",
-            position: "top-center",
-          });
-        }
-        return (
-          <Button
-            onClick={copy}
-            size="xs"
-            rightSection={copied ? <LuCheck /> : <LuShare />}
-          >
-            Share
-          </Button>
-        );
+    <Button
+      onClick={async () => {
+        const d = await encode(definition, COMPRESSION_METHODS.GZIP);
+        const s = await encode(schema, COMPRESSION_METHODS.GZIP);
+        const value = `${host}/flows-ide?d=${d}&s=${s}&format=gzip`;
+        clipboard.copy(value);
       }}
-    </CopyButton>
+      size="xs"
+      rightSection={clipboard.copied ? <LuCheck /> : <LuShare />}
+    >
+      Share
+    </Button>
   );
 }
